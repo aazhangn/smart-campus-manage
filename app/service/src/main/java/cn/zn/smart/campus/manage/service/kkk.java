965c600b6dc8a6868cf9cb37df3afe955d7b1bd4
zhangnan <1976328529@qq.com>
2021-05-11 18:03:32 +0800

    feat：项目搭建

---
+++ app/service/src/main/java/cn/zn/smart/campus/manage/service/kkk.java
@@ -0,0 +1,9 @@
+package cn.zn.smart.campus.manage.service;
+
+/**
+ * @Description: kkk
+ * @Author: zhangnan
+ * @Date: 2021/05/11 16:28
+ */
+public class kkk {
+}
---
+++ app/web/src/main/java/cn/zn/smart/campus/manage/web/WebApplication.java
@@ -0,0 +1,14 @@
+package cn.zn.smart.campus.manage.web;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ *
+ * @Author: zhangnan
+ * @Date: 2021/05/11 16:23
+ */
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
