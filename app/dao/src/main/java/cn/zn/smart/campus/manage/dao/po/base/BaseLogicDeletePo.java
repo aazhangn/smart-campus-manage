fae9ed1c0eb2b8e474c9820bb9b2344a58d6f526
zhangnan <1976328529@qq.com>
2021-05-14 00:12:07 +0800

    #

--- app/dao/src/main/java/cn/zn/smart/campus/manage/dao/po/base/BaseLogicDeletePo.java
+++ app/dao/src/main/java/cn/zn/smart/campus/manage/dao/po/base/BaseLogicDeletePo.java
@@ -1,12 +1,14 @@
 package cn.zn.smart.campus.manage.dao.po.base;
 
 import com.baomidou.mybatisplus.annotation.TableLogic;
+import lombok.Data;
 
 /**
  * @Description: 逻辑删除基础po
  * @Author: zhangnan
  * @Date: 2021/05/13 11:19
  */
+@Data
 public abstract class BaseLogicDeletePo extends BasePo{
     private static final long serialVersionUID = -9050204997701276208L;
 
--- app/dao/src/main/java/cn/zn/smart/campus/manage/dao/po/base/BasePo.java
+++ app/dao/src/main/java/cn/zn/smart/campus/manage/dao/po/base/BasePo.java
@@ -3,6 +3,7 @@ package cn.zn.smart.campus.manage.dao.po.base;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.extension.activerecord.Model;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -12,6 +13,7 @@ import java.util.Date;
  * @Author: zhangnan
  * @Date: 2021/05/13 11:04
  */
+@Data
 public abstract class BasePo extends Model<BasePo> implements Serializable {
 
     private static final long serialVersionUID = -6110217049640557173L;
